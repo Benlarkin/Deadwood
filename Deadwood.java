c4a5fc6ac01e83b0fdc5a42ba89ad942bf53c61a
kleinm6 <kleinm6>
2019-07-09 15:55:31 -0700

    Putting in Deadwood and Player classes.

---
+++ Deadwood.java
@@ -0,0 +1,19 @@
+public class Deadwood {
+  public List<Player> players;
+  public Board board;
+  public Card[] deck;
+  public Banker banker;
+  private Timer timer;
+
+  public Deadwood() {
+
+  }
+
+  public void addPlayer(Player newPlayer) {
+
+  }
+
+  public void startGame() {
+
+  }
+}
---
+++ Player.java
@@ -0,0 +1,63 @@
+public class Player {
+  private String name;
+  private int dollars;
+  private int credits;
+  private int rank;
+  private int rehearsalChips;
+  private Room currentRoom;
+  private Role currentRole;
+
+  public Player(String name) {
+
+  }
+
+  public Room move() {
+    
+  }
+
+  public int countScore() {
+
+  }
+
+  public String getName() {
+
+  }
+
+  public int getDollars() {
+
+  }
+
+  public int getCredits() {
+
+  }
+
+  public int getRank() {
+
+  }
+
+  public Room getCurrentRoom() {
+
+  }
+
+  public Role getCurrentRole() {
+
+  }
+
+  public void setDollars() {
+
+  }
+
+  public void setCredits() {
+
+  }
+
+  public void setRank() {
+
+  }
+
+  public void setCurrentRole() {
+
+  }
+
+
+}
