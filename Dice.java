59db106efca19ebe8d186112034a7fb158152156
Benjamin Larkin <Ben>
2019-07-12 14:59:24 -0700

    added dice roll function

--- Dice.java
+++ Dice.java
@@ -2,14 +2,15 @@ import java.util.*;
 
 public class Dice extends Graphic {
 
-  private Random rand;
+  private Random rand = new Random();
 
   public Dice() {
 
   }
 
+  // rolls number between 1-6 then adds the rehearsal points (aka additions)
   public int roll(int additions) {
-	  return -1;
+    return 1 + rand.nextInt(6) + additions;
   }
 
 
