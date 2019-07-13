a9ae0d8ec602979979c13359a5635683cd5179fc
Benjamin Larkin <Ben>
2019-07-13 11:40:09 -0700

    finished promotion functionality, working ont esting

--- CastingOffice.java
+++ CastingOffice.java
@@ -14,26 +14,35 @@ public class CastingOffice extends Room {
 
   // Scanner must be system.in
   // it is ok if player is max rank
-  public int promote(Player player, Scanner input) {
+  // promote player via private function OR return "insufficient currency"
+  // (put this function inside of loop during player turn?)
+  public void promote(Player player, Scanner input) {
     if (player.getRank() == 6) {
       System.out.println("You are already max rank!");
-      return 6;
+      return;
     }
     if (player.getRank() == 5) {
       System.out.println("What rank would you like to promote to? (6). You are currently rank: " + player.getRank());
     }
     System.out.println("What rank would you like to promote to? (" + (player.getRank() + 1)
         + "-6). You are currently rank: " + player.getRank());
-    int response = Integer.parseInt(input.next());
+    int desiredRank = Integer.parseInt(input.next());
     System.out.println("What currency will you use to upgrade? (d/c)");
     System.out.println("You currently have " + player.getDollars() + "dollars, and " + player.getCredits() + "credits");
     // check if sufficient currency
-    // promote player via private function OR return "insufficient currency"
-    return -1;
+    String desiredCurrency = input.next();
+    if(currencyCheck(player, desiredRank, desiredCurrency)){
+      promote(player, desiredRank);
+    }
+    else{
+      System.out.println("Insufficient Currency");
+    }
   }
 
-  private int promote(Player player, int currency) {
-    return -1;
+  private int promote(Player player, int desiredRank) {
+    player.setRank(desiredRank);
+    // return players current rank for GUI
+    return player.getRank();
   }
 
   private int[][] setUpgradeCost() {
@@ -51,6 +60,39 @@ public class CastingOffice extends Room {
     a[1][3] = 20;
     a[1][4] = 25;
     return a;
+  }
+
+  private boolean currencyCheck(Player player, int desiredRank, String desiredCurrency) {
+    // adjust number for cost array
+    desiredRank -= 2;
+    // credits case
+    if (desiredCurrency == "c") {
+      int requiredCredits = cost[desiredRank][1];
+      if (checkCredits(player, requiredCredits)) {
+        return true;
+      }
+      // dollars case
+    } else {
+      int requiredDollars = cost[desiredRank][0];
+      if (checkDollars(player, requiredDollars)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean checkCredits(Player player, int requiredCredits) {
+    if (player.getCredits() < requiredCredits) {
+      return false;
+    }
+    return true;
+  }
+
+  private boolean checkDollars(Player player, int requiredDollars) {
+    if (player.getDollars() < requiredDollars) {
+      return false;
+    }
+    return true;
   }
 
 }
