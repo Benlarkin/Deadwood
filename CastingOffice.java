9999a3caf1b537d1890f7158387830d080908688
Benjamin Larkin <benlarkin.usa@gmail.com>
2019-07-18 15:47:07 -0700

    updated string implementation

--- CastingOffice.java
+++ CastingOffice.java
@@ -1,7 +1,17 @@
 import java.util.*;
 
 public class CastingOffice extends Room {
-	private final String OFFICE = "office";
+  private final String MAXRANK = "You are already max rank!";
+  private final String OFFICE = "office";
+  private final String RANKREQUEST5 = "What rank would you like to promote to? (6). You are currently rank: ";
+  private final String RANKREQUEST = "What rank would you like to promote to? (";
+  private final String RANKREQUEST_PT2 = "-6). You are currently rank: ";
+  private final String CURRENCYMSG1 = "You currently have ";
+  private final String CURRENCYMSG2 = " dollars, and ";
+  private final String CURRENCYMSG3 = " credits";
+  private final String DESIREDCURRENCYMSG = "What currency will you use to upgrade? (d/c)";
+  private final String INSUFFICIENTCURRENCY = "Insufficient Currency";
+  private final String CREDITS_AS_STRING = "c";
   // cost of upgrades, cost[0][x] = dollars, cost[1][x] = credits
   // cost[x][0] = rank 2, cost[x][4] = rank 6;
   // cost[1][0] is price to upgrade to rank 2 in credits
@@ -20,27 +30,27 @@ public class CastingOffice extends Room {
   // (put this function inside of loop during player turn?)
   public void promote(Player player, Scanner input) {
     if (player.getRank() == 6) {
-      System.out.println("You are already max rank!");
+      System.out.println(MAXRANK);
       return;
     }
     if (player.getRank() == 5) {
-      System.out.println("What rank would you like to promote to? (6). You are currently rank: " + player.getRank());
+      System.out.println(RANKREQUEST5 + player.getRank());
     } else {
-      System.out.println("What rank would you like to promote to? (" + (player.getRank() + 1)
-          + "-6). You are currently rank: " + player.getRank());
+      System.out.println(RANKREQUEST + (player.getRank() + 1)
+          + RANKREQUEST_PT2 + player.getRank());
     }
     System.out
-        .println("You currently have " + player.getDollars() + " dollars, and " + player.getCredits() + " credits");
+        .println(CURRENCYMSG1 + player.getDollars() + CURRENCYMSG2 + player.getCredits() + CURRENCYMSG3);
     representCost();
     // print a pretty array with leveling costs here... do later
     int desiredRank = Integer.parseInt(input.next());
-    System.out.println("What currency will you use to upgrade? (d/c)");
+    System.out.println(DESIREDCURRENCYMSG);
     // check if sufficient currency
     String desiredCurrency = input.next();
     if (currencyCheck(player, desiredRank, desiredCurrency)) {
       promote(player, desiredRank, desiredCurrency);
     } else {
-      System.out.println("Insufficient Currency");
+      System.out.println(INSUFFICIENTCURRENCY);
     }
   }
 
@@ -50,7 +60,7 @@ public class CastingOffice extends Room {
     // adjust number for cost array
     desiredRank -= 2;
     // deduct credits
-    if (desiredCurrency.equals("c")) {
+    if (desiredCurrency.equals(CREDITS_AS_STRING)) {
       int requiredCredits = cost[1][desiredRank];
       banker.takeCredits(player, requiredCredits);
     } else {
@@ -83,7 +93,7 @@ public class CastingOffice extends Room {
     // adjust number for cost array
     desiredRank -= 2;
     // credits case
-    if (desiredCurrency.equals("c")) {
+    if (desiredCurrency.equals(CREDITS_AS_STRING)) {
       int requiredCredits = cost[1][desiredRank];
       if (checkCredits(player, requiredCredits)) {
         return true;
