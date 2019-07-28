d8ebc0138049325d10e5142bd8ab0b85531670ba
kleinm6 <kleinm6>
2019-07-28 14:00:30 -0700

    Added functionality to some buttons.

---
+++ Main.java
@@ -0,0 +1,21 @@
+import Observer.*;
+
+
+public class Main {
+
+  // Creates a new game of Deadwood, then starts it.
+  public static void main(String[] args) {
+	  if(args.length != 1) {
+		  System.out.println("ERROR");
+	  }
+	  else {
+		  try {
+        Observer observer = Observer.getObserver();
+        observer.startGame(Integer.parseInt(args[0]));
+		  }
+		  catch(Exception e) {
+			  System.out.println(e);
+		  }
+	  }
+  }
+}
