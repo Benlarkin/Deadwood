c296eda1487b1ffb83d178ea3afa6926b4c24d6d
Benjamin Larkin <benlarkin.usa@gmail.com>
2019-07-28 12:24:57 -0700

    removed duplicates x2

--- MoveButtonMouseListener.java
+++
@@ -1,9 +0,0 @@
-import java.awt.event.MouseEvent;
-
-public class MoveButtonMouseListener extends BaseDeadwoodMouseListener {
-    private final String MOVE_SELECT_MSG = "Move is Selected\n";
-
-    public void mouseClicked(MouseEvent e) {
-        System.out.println(MOVE_SELECT_MSG);
-    }
-}
