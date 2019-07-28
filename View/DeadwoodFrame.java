25bc011c6812f97e9df0be84e824bdae397f0a27
Benjamin Larkin <benlarkin.usa@gmail.com>
2019-07-28 13:48:55 -0700

    initialized playerdice inside of trailers

--- View/DeadwoodFrame.java
+++ View/DeadwoodFrame.java
@@ -1,4 +1,5 @@
 package View;
+
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.ImageIcon;
@@ -10,6 +11,14 @@ public class DeadwoodFrame extends JFrame {
     private JLabel labelCard;
     private JLabel labelPlayer;
     private JLabel labelMenu;
+    private JLabel label_p1;
+    private JLabel label_p2;
+    private JLabel label_p3;
+    private JLabel label_p4;
+    private JLabel label_p5;
+    private JLabel label_p6;
+    private JLabel label_p7;
+    private JLabel label_p8;
 
     private JButton buttonAct;
     private JButton buttonRehearse;
@@ -22,14 +31,12 @@ public class DeadwoodFrame extends JFrame {
     private static final String DEADWOOD_TITLE = "Deadwood";
     private static final String GAME_BOARD_IMAGE = "images/board.jpg";
     private static final String CARD_IMAGE = "images/01.png";
-    private static final String DICE_IMAGE = "images/r2.png";
+    private static final String DICE_IMAGE = "images/r1.png";
     private static final String MENU_LABEL_TEXT = "MENU";
     private static final String ACT_BUTTON_TEXT = "ACT";
     private static final String REHEARSE_BUTTON_TEXT = "REHEARSE";
     private static final String MOVE_BUTTON_TEXT = "MOVE";
     protected static final String PLAYERMSG = "Player %d name: ";
-
-
 
     public DeadwoodFrame() {
         super(DEADWOOD_TITLE);
@@ -39,11 +46,9 @@ public class DeadwoodFrame extends JFrame {
         initializeDeadwoodPane();
     }
 
-
     private void initializeLabels() {
         setupGameBoardLabel();
         setupCardLabel();
-        setupPlayerLabel();
         setupMenuLabel();
     }
 
@@ -63,12 +68,61 @@ public class DeadwoodFrame extends JFrame {
         labelCard.setOpaque(true);
     }
 
-    private void setupPlayerLabel() {
-        labelPlayer = new JLabel();
-        ImageIcon playerDiceIcon = new ImageIcon(DICE_IMAGE);
-        labelPlayer.setIcon(playerDiceIcon);
-        labelPlayer.setBounds(114, 227, playerDiceIcon.getIconWidth(), playerDiceIcon.getIconHeight());
-        //labelPlayer.setBounds(114,227,46,46);
+    // SET UP Player dice in trailers at start of game
+    public void initDice(int playerCount) {
+        paneDeadwood = getLayeredPane();
+        label_p1 = new JLabel();
+        ImageIcon icon1 = new ImageIcon("images/dice/b1.png");
+        label_p1.setIcon(icon1);
+        label_p2 = new JLabel();
+        ImageIcon icon2 = new ImageIcon("images/dice/c1.png");
+        label_p2.setIcon(icon2);
+        label_p1.setBounds(1030, 400, 46, 46);
+        label_p2.setBounds(1030, 350, 46, 46);
+        paneDeadwood.add(label_p1, new Integer(3));
+        paneDeadwood.add(label_p2, new Integer(3));
+        if (playerCount >= 3) {
+            label_p3 = new JLabel();
+            ImageIcon icon3 = new ImageIcon("images/dice/g1.png");
+            label_p3.setIcon(icon3);
+            label_p3.setBounds(1030, 300, 46, 46);
+            paneDeadwood.add(label_p3, new Integer(3));
+        }
+        if (playerCount >= 4) {
+            label_p4 = new JLabel();
+            ImageIcon icon4 = new ImageIcon("images/dice/o1.png");
+            label_p4.setIcon(icon4);
+            label_p4.setBounds(1030, 250, 46, 46);
+            paneDeadwood.add(label_p4, new Integer(3));
+        }
+        if (playerCount >= 5) {
+            label_p5 = new JLabel();
+            ImageIcon icon4 = new ImageIcon("images/dice/p1.png");
+            label_p5.setIcon(icon4);
+            label_p5.setBounds(1120, 250, 46, 46);
+            paneDeadwood.add(label_p5, new Integer(3));
+        }
+        if (playerCount >= 6) {
+            label_p6 = new JLabel();
+            ImageIcon icon4 = new ImageIcon("images/dice/r1.png");
+            label_p6.setIcon(icon4);
+            label_p6.setBounds(1120, 300, 46, 46);
+            paneDeadwood.add(label_p6, new Integer(3));
+        }
+        if (playerCount >= 7) {
+            label_p7 = new JLabel();
+            ImageIcon icon4 = new ImageIcon("images/dice/v1.png");
+            label_p7.setIcon(icon4);
+            label_p7.setBounds(1120, 350, 46, 46);
+            paneDeadwood.add(label_p7, new Integer(3));
+        }
+        if (playerCount == 8) {
+            label_p8 = new JLabel();
+            ImageIcon icon4 = new ImageIcon("images/dice/y1.png");
+            label_p8.setIcon(icon4);
+            label_p8.setBounds(1120, 400, 46, 46);
+            paneDeadwood.add(label_p8, new Integer(3));
+        }
     }
 
     private void setupMenuLabel() {
@@ -107,8 +161,8 @@ public class DeadwoodFrame extends JFrame {
         paneDeadwood = getLayeredPane();
         paneDeadwood.add(labelGameBoard, new Integer(0)); // Add the board to the lowest layer
         paneDeadwood.add(labelCard, new Integer(1)); // Add the card to the lower layer
-        paneDeadwood.add(labelPlayer, new Integer(3));
-        paneDeadwood.add(labelMenu, new Integer(2));
+        // paneDeadwood.add(label1, new Integer(3)); // add dice
+        paneDeadwood.add(labelMenu, new Integer(2)); // add menu
 
         paneDeadwood.add(buttonAct, new Integer(2));
         paneDeadwood.add(buttonRehearse, new Integer(2));
@@ -116,73 +170,58 @@ public class DeadwoodFrame extends JFrame {
     }
 
     public List<JTextField> makeTextPanel(int playerNum) {
-    	JPanel panel = new JPanel();
-    	List<JTextField> list = new ArrayList<JTextField>();
-    	panel.setLayout(new SpringLayout());
-    	panel.setPreferredSize(new Dimension(300, 300));
-    	for(int i = 0; i < playerNum; i++) {
-    		// JLabel label = new JLabel(PLAYERMSG, i+1);
-    		JTextField textBox = new JTextField(20);
-    		//label.setLabelFor(textBox);
-    		//panel.add(label);
-    		panel.add(textBox);
-    		list.add(textBox);
-    	}
-    	this.getContentPane().add(panel);
-    	this.setVisible(true);
-    	return list;
+        JPanel panel = new JPanel();
+        List<JTextField> list = new ArrayList<JTextField>();
+        panel.setLayout(new SpringLayout());
+        panel.setPreferredSize(new Dimension(300, 300));
+        for (int i = 0; i < playerNum; i++) {
+            // JLabel label = new JLabel(PLAYERMSG, i+1);
+            JTextField textBox = new JTextField(20);
+            // label.setLabelFor(textBox);
+            // panel.add(label);
+            panel.add(textBox);
+            list.add(textBox);
+        }
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+        return list;
     }
 
     // public JButton getActButton() {
-    //   return buttonAct;
+    // return buttonAct;
     // }
     // public JButton getRehearseButton() {
-    //   return buttonRehearse;
+    // return buttonRehearse;
     // }
     // public JButton getMoveButton() {
-    //   return moveButton;
+    // return moveButton;
     // }
 
-
-
-
     public List<String> getNameInput(int playerNum) {
-      List<String> names = new ArrayList<String>();
-      for(int i = 0; i < playerNum; i++) {
-        names.add(getPlayerName(i+1));
-      }
-      return names;
+        List<String> names = new ArrayList<String>();
+        for (int i = 0; i < playerNum; i++) {
+            names.add(getPlayerName(i + 1));
+        }
+        return names;
     }
 
     // Adds players to the game of Deadwood.
     private String getPlayerName(int playerNum) {
-        String name = (String)JOptionPane.showInputDialog(
-                            this,
-                            "Enter Name for Player " + playerNum + ":\n",
-                            "Player " + playerNum + " Name",
-                            JOptionPane.PLAIN_MESSAGE,
-                            null,
-                            null,
-                            null);
-        if(name != null && name.length() > 0) {
-          return name;
+        String name = (String) JOptionPane.showInputDialog(this, "Enter Name for Player " + playerNum + ":\n",
+                "Player " + playerNum + " Name", JOptionPane.PLAIN_MESSAGE, null, null, null);
+        if (name != null && name.length() > 0) {
+            return name;
         }
         return retryGetPlayerName(playerNum);
     }
 
     private String retryGetPlayerName(int playerNum) {
-      String name = (String)JOptionPane.showInputDialog(
-                          this,
-                          "Enter Name for Player " + playerNum + ":\n" +
-                          "Error: Please Enter Name!\n",
-                          "Player " + playerNum + " Name",
-                          JOptionPane.PLAIN_MESSAGE,
-                          null,
-                          null,
-                          null);
-      if(name != null && name.length() > 0) {
-        return name;
-      }
-      return retryGetPlayerName(playerNum);
+        String name = (String) JOptionPane.showInputDialog(this,
+                "Enter Name for Player " + playerNum + ":\n" + "Error: Please Enter Name!\n",
+                "Player " + playerNum + " Name", JOptionPane.PLAIN_MESSAGE, null, null, null);
+        if (name != null && name.length() > 0) {
+            return name;
+        }
+        return retryGetPlayerName(playerNum);
     }
 }
